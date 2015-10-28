65f2f4cc3f99dfe079f5100e9b58f7d90f49696b
Phil Webster <pwebster33@gmail.com>
2015-10-27 23:11:18 -0400

    Version 0.3-  removed @imports for Google fonts  from CSS.  Used is_numeric to verify only numbers where being used.

--- includes/so_widget.php
+++ includes/so_widget.php
@@ -5,11 +5,11 @@ class SO_Widget extends WP_Widget {
 	function __construct() {
 		$widget_ops = array(
 			'classname' => 'SO_Widget_Class',
-			'description' => 'Widget to display odometer');
-		parent::__construct('SO_Widget', 'Odometer Widget', $widget_ops);
+			'description' => 'Widget to display odometer' );
+		parent::__construct( 'SO_Widget', 'Odometer Widget', $widget_ops );
 	}
 
-	function form($instance) {
+	function form( $instance ) {
 		$defaults = array(
 			'odometer_style' => 'Default',
 			'start_num' => 123,
@@ -19,59 +19,60 @@ class SO_Widget extends WP_Widget {
 			);
 		$instance = wp_parse_args( (array) $instance, $defaults );
 		$odometer_style = $instance[ 'odometer_style' ];
-		$start_num = $instance['start_num'];
-		$end_num = $instance['end_num'];		
-		$delay_time = $instance['delay_time'];
-		$font_size = $instance['font_size'];
-		$before_text = $instance['before_text'];
-		$after_text = $instance['after_text'];
+		$start_num = $instance[ 'start_num' ];
+		$end_num = $instance[ 'end_num' ];		
+		$delay_time = $instance[ 'delay_time' ];
+		$font_size = $instance[ 'font_size' ];
+		$before_text = $instance[ 'before_text' ];
+		$after_text = $instance[ 'after_text' ];
 		
 		$html = '<p>Odometer Style:';
-			$html .= '<select name=" ' . $this->get_field_name('odometer_style') . '">';
-				$html .= '<option value="default"'. selected( $odometer_style, 'default', false ). '>Default</option>';
-				$html .= '<option value="car"'. selected( $odometer_style, 'car', false ). '>Car</option>';
-				$html .= '<option value="digital"'. selected( $odometer_style, 'digital', false ). '>Digital</option>';
-				$html .= '<option value="minimal"'. selected( $odometer_style, 'minimal', false ). '>Minimal</option>';
-				$html .= '<option value="plaza"'. selected( $odometer_style, 'plaza', false ). '>Plaza</option>';
-				$html .= '<option value="slot"'. selected( $odometer_style, 'slot', false ). '>Slot Machine</option>';
-				$html .= '<option value="train"'. selected( $odometer_style, 'train', false ). '>Train Station</option>';
+			$html .= '<select name=" ' . $this->get_field_name( 'odometer_style' ) . '">';
+				$html .= '<option value="default"'. selected( $odometer_style, 'default', false ). 	'>Default</option>';
+				$html .= '<option value="car"'. 	selected( $odometer_style, 'car', false ). 		'>Car</option>';
+				$html .= '<option value="digital"'. selected( $odometer_style, 'digital', false ). 	'>Digital</option>';
+				$html .= '<option value="minimal"'. selected( $odometer_style, 'minimal', false ). 	'>Minimal</option>';
+				$html .= '<option value="plaza"'. 	selected( $odometer_style, 'plaza', false ). 	'>Plaza</option>';
+				$html .= '<option value="slot"'. 	selected( $odometer_style, 'slot', false ). 	'>Slot Machine</option>';
+				$html .= '<option value="train"'. 	selected( $odometer_style, 'train', false ). 	'>Train Station</option>';
 			$html .= '</select>';
+		$html .= '</p>';
 		$html .= '<p>Start Number:';
 			$html .= '<input class="widefat"
-						name="'. $this->get_field_name('start_num') . '"
-						type="text" value="'. esc_attr($start_num) . '" />';
+						name="'. $this->get_field_name( 'start_num' ) . '"
+						type="text" value="'. esc_attr( $start_num ) . '" />';
 		$html .= '</p>'; 
 
 		$html .= '<p>End Number:';
 			$html .= '<input class="widefat"
-						name="' . $this->get_field_name('end_num') . '"
-						type="text" value="' . esc_attr($end_num) . '" />';
+						name="' . $this->get_field_name( 'end_num' ) . '"
+						type="text" value="' . esc_attr( $end_num ) . '" />';
 		$html .= '</p>';
 
 		
 		$html .= '<p>Delay Time (in seconds ie. 1.5):';
 			$html .= '<input class="widefat"
-						name="' . $this->get_field_name('delay_time') . '"
-						type="text" value="' . esc_attr($delay_time) . '" />';
+						name="' . $this->get_field_name( 'delay_time' ) . '"
+						type="text" value="' . esc_attr( $delay_time ) . '" />';
 		$html .= '</p>';
 
 		$html .= '<p>Font Size (in pixels ie. 18):';
 			$html .= '<input class="widefat"
-						name="' . $this->get_field_name('font_size'). ' "
-						type="text" value="' . esc_attr($font_size) . '" />';
+						name="' . $this->get_field_name( 'font_size' ) . ' "
+						type="text" value="' . esc_attr( $font_size ) . '" />';
 		$html .= '</p>';
 
 		$html .= '<p>Text Before Odometer:';
 			$html .= '<textarea class="widefat" rows="5" cols="20"
-						name=" ' . $this->get_field_name('before_text').'" >';
-				$html.= esc_attr($before_text);
+						name=" ' . $this->get_field_name( 'before_text' ) . '" >';
+				$html.= esc_attr( $before_text );
 			$html .= '</textarea>';
 		$html .= '</p>';
 
 		$html .= '<p>Text After Odometer:';
 			$html .= '<textarea class="widefat" rows="5" cols="20"
-						name="' . $this->get_field_name('after_text') . '" >';
-				$html .= esc_attr($after_text);
+						name="' . $this->get_field_name( 'after_text' ) . '" >';
+				$html .= esc_attr( $after_text );
 			$html .= '</textarea>';
 		$html .= '</p>';			 			 
 				 						
@@ -79,25 +80,30 @@ class SO_Widget extends WP_Widget {
 	}
 
 
-		function update( $new_instance, $old_instance)  {
-			$instance = $old_instance;
-			$instance['odometer_style'] = strip_tags( $new_instance['odometer_style']);
-			$instance['start_num'] = sanitize_text_field( $new_instance['start_num']);
-			$instance['end_num'] = sanitize_text_field( $new_instance['end_num']);
-			$instance['delay_time'] = sanitize_text_field( $new_instance['delay_time']);
-			$instance['font_size'] = sanitize_text_field( $new_instance['font_size']);
-			$instance['before_text'] =  stripslashes( wp_filter_post_kses( addslashes($new_instance['before_text']) ) );
-			$instance['after_text'] = stripslashes( wp_filter_post_kses( addslashes($new_instance['after_text']) ) );
-			return $instance;
-		}
+	function update( $new_instance, $old_instance )  {
+		$instance = $old_instance;
+		$instance[ 'odometer_style' ] = strip_tags( $new_instance[ 'odometer_style' ] );
+		if ( is_numeric( $new_instance[ 'start_num' ] ) ) 
+			$instance[ 'start_num' ] = sanitize_text_field( $new_instance[ 'start_num' ] );
+		if ( is_numeric( $new_instance[ 'end_num' ] ) )
+			$instance[ 'end_num' ] = sanitize_text_field( $new_instance[ 'end_num' ] );
+		if ( is_numeric( $new_instance[ 'delay_time'] ) )
+			$instance[ 'delay_time' ] = sanitize_text_field( $new_instance[ 'delay_time' ] );
+		if ( is_numeric( $new_instance[ 'font_size' ] ) )
+			$instance[ 'font_size' ] = sanitize_text_field( $new_instance[ 'font_size' ] );
+		$instance[ 'before_text' ] =  stripslashes( wp_filter_post_kses( addslashes( $new_instance[ 'before_text' ] ) ) );
+		$instance[ 'after_text' ] = stripslashes( wp_filter_post_kses( addslashes( $new_instance[ 'after_text' ] ) ) );
+		
+		return $instance;
+	}
 
 		
 
 
-		 function widget( $args, $instance){
+		 function widget( $args, $instance ) {
 			extract( $args );
-			extract( $instance);
-
+			extract( $instance );
+			$html;
 			//Enqueue styles and scripts
 			$odometer_styles = array(
 				'car' 		=>	'car.css',
@@ -106,36 +112,45 @@ class SO_Widget extends WP_Widget {
 				'minimal' 	=> 	'minimal.css',
 				'plaza' 	=> 	'plaza.css',
 				'slot' 		=> 	'slot-machine.css',
-				'train'	 	=> 	'train-station.css');
+				'train'	 	=> 	'train-station.css' );			
 
-			wp_enqueue_style( 'so-styles', SO_PLUGIN_URL . 'assests/css/odometer-theme-' . $odometer_styles[$odometer_style]);
+			wp_enqueue_style( 'so-styles', SO_PLUGIN_URL . 'assets/css/odometer-theme-' . $odometer_styles[ $odometer_style ] );
 			
-			wp_enqueue_script( 'so-odometer', SO_PLUGIN_URL . 'assests/js/odometer.js',	array( ));
-			
-			
+			$odometer_fonts = array(
+				'car' => '//fonts.googleapis.com/css?family=Arimo',
+				'digital' => '//fonts.googleapis.com/css?family=Wallpoet',
+				'slot' => '//fonts.googleapis.com/css?family=Rye',
+				'train' => '//fonts.googleapis.com/css?family=Economica'
+				);
 
-			wp_enqueue_script( 'so-script', SO_PLUGIN_URL . 'assests/js/so-script.js', array( 'jquery', 'so-odometer'));
+			if ( array_key_exists( $odometer_style, $odometer_fonts ) )
+				wp_enqueue_style( 'so-font', $odometer_fonts[ $odometer_style ] );
+			wp_enqueue_script( 'so-odometer', SO_PLUGIN_URL . 'assets/js/odometer.js',	array( ) , false, true );			
+
+			wp_enqueue_script( 'so-script', SO_PLUGIN_URL . 'assets/js/so-script.js', array( 'jquery', 'so-odometer' ), false, true );
 			
 			// variables to send to javascript
 			$so_script_vars = array(
 						'delay_time' => $delay_time,
-						'end_num' => $end_num);
+						'end_num' => $end_num 
+						);
 			wp_localize_script( 'so-script', 'so_script_vars', $so_script_vars);
 
-			echo $before_widget;
+			$html .= $before_widget;
 			if( $before_text ) {
-				echo '<div>' . $before_text. '</div>';
+				$html .= '<div>' . $before_text . '</div>';
 			}
 			
-			echo '<div class="odometer" id="example1" style="font-size: '. $font_size.'px; margin: 0 auto;"> '
+			$html .= '<div class="odometer" id="example1" style="font-size: '. $font_size .'px; margin: 0 auto;"> '
                 . $start_num . ' </div>';
 		   	
 		  	
-			if ( $after_text) {
-				echo '<div>'. $after_text . '</div>';
+			if ( $after_text ) {
+				$html .= '<div>'. $after_text . '</div>';
 			}			
 			
-			echo $after_widget;
+			$html .= $after_widget;
+			echo $html;
 		}
 	} 
  
--- spinning-odometer.php
+++ spinning-odometer.php
@@ -3,13 +3,11 @@
 Plugin Name: Spinning Odometer
 Plugin URI:  http://philwp.com/spinning-odometer
 Description: Displays a number like an odometer with spinning numbers
-Version:     0.2
+Version:     0.3
 Author:     Phil Webster	
 Author URI:  http://philwp.com
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Domain Path: /languages
-Text Domain: my-toolset
 */
 
 /*
@@ -19,17 +17,17 @@ Text Domain: my-toolset
 */
 
 // plugin folder url
-if(!defined('SO_PLUGIN_URL')) {
-	define('SO_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+if( !defined( 'SO_PLUGIN_URL' )) {
+	define( 'SO_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 }
 // plugin folder path
-if(!defined('SO_PLUGIN_DIR')) {
-	define('SO_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+if( !defined( 'SO_PLUGIN_DIR' )) {
+	define( 'SO_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
 }
 // plugin root file
-if(!defined('SO_PLUGIN_FILE')) {
-	define('SO_PLUGIN_FILE', __FILE__);
-}
+if( !defined( 'SO_PLUGIN_FILE' )) {
+	define( 'SO_PLUGIN_FILE', __FILE__ );
+}  
 
 /*
 |--------------------------------------------------------------------------
@@ -37,7 +35,7 @@ if(!defined('SO_PLUGIN_FILE')) {
 |--------------------------------------------------------------------------
 */
 
-include_once( SO_PLUGIN_DIR. '/includes/so_widget.php');
+include_once( SO_PLUGIN_DIR. '/includes/so_widget.php' );
 
 /*
 |--------------------------------------------------------------------------
@@ -45,9 +43,9 @@ include_once( SO_PLUGIN_DIR. '/includes/so_widget.php');
 |--------------------------------------------------------------------------
 */
 
-function so_register_widgets() {
+function so_register_widget() {
 	register_widget( 'SO_Widget' );
 }
 
 // hook using widget_init
-add_action( 'widgets_init', 'so_register_widgets' );
+add_action( 'widgets_init', 'so_register_widget' );
